cd25d3722a65523e5cfd44c2aec955412edb9f81
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-12-26 10:09:15 -0300

    create fake notification repository for test

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,16 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+//Criando um repositório fake
+const notificationsRepository = {
+  async create(notification: Notification){
+    console.log(notification);
+  }
+}
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification()
+    const sendNotification = new SendNotification(notificationsRepository)
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
